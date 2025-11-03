d9313715ae27a20dd8eeee9c51cc43d8da9b58f4
sofia villela <sofiavillelav@gmail.com>
2025-11-03 17:40:01 -0300

    crudzinho das tarefas e custo

---
+++ temptarefas.py
@@ -0,0 +1,14 @@
+def tarefas(tarefas_evento):
+    
+    print("\n--- Tarefas e do Evento ---")
+    decoracao = input("Decoração do evento: ").strip()
+    buffet = input("Buffet: ").strip()
+    musica_evento = input("Música do evento: ").strip()
+    """ pode ter mais opçoes """
+    
+
+def custo_tarefas(custo_tarefas_evento):
+    custo_decoracao = float(input("Custo da decoração:"))    
+    custo_buffet = float(input("Custo do buffet:"))
+    custo_musica_evento = float(input("Custo da música do evento:"))
+    """ pode ter mais opçoes """
